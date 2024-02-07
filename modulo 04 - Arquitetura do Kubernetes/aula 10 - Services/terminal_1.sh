1ecabc33157f64def3bc26be18e0ed6053ed859b
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2024-02-07 19:40:35 -0300

    feat: services comentarios

--- modulo 04 - Arquitetura do Kubernetes/aula 10 - Services/terminal_1.sh
+++ modulo 04 - Arquitetura do Kubernetes/aula 10 - Services/terminal_1.sh
@@ -4,4 +4,5 @@ kubectl create -f deployment.yaml
 kubectl get po -o wide
 
 kubectl create -f service.yaml
-kubectl port-forward service/service-demo-service 4200:80
+kubectl port-forward service/service-demo-service 4200:80 #Consegue expor no browser o service na porta 4200 
+                                                          #e fazer redirecionamento para o pod na porta 80
